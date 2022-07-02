8fa705a692124c93ed551751ab66bb687ed078ad
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 09:59:32 -0700

    last changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//main - prints a phrase and returns 0 if exited properly
+/*** main - prints a phrase and returns 0 if exited properly*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
