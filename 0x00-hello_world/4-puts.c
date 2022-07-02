460dcb86dcf9f59285ed43ffe3d454bb06bec655
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 08:38:56 -0700

    printing phrases

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+/** * main - prints the words "Programming is like building a multilingual puzzle" * * Return: 0 if excited properly (Success) */ int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }     
