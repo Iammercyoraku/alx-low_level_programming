b7c087ed239ca14ec1f317f5e49b651e09a47805
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 23:36:00 -0700

    little fixes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - prints with proper grammar, but the outcome is a piece of art
-* return: 0
+* Return: 0
 */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
