16d3af8710fc7ff2e7715c134709ab22e9bc6882
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 22:38:49 -0700

    early morning fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** * main - prints the phrase "Programming is like building a multilingual puzzle * Return: 0 */
+/** * main - prints "Programming is like building a multilingual puzzle * Return: 0 */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
