7fe39f1235e05789335dc3db12647514d9c31e0d
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 23:43:10 -0700

    script that generates the assembly code (Intel syntax) of a C code and save it in an output file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-/** * main - main - prints the phrase "Programming is like building a multilingual puzzle * Return: Always 0 (Success) */ int main(void) { return (0); }
+/** * main - Entry point * Return: Always 0 (Success) */ int main(void) { return (0); }
