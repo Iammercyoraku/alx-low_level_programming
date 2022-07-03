6f968c5aada5dc7a698985d5fc2c43ec837a30ee
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 23:37:35 -0700

    little fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/** * main - prints "Programming is like building a multilingual puzzle */
+/**
+* main - prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
