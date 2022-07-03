81b0515981b3e046127e52cc170fa19a01ab13fd
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-02 23:28:49 -0700

    print sizes of various computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
-#include <stdio.h> /** * main - prints the size of various types * Return: 0 */ 
+#include <stdio.h>
+/** * main - prints the size of various types * Return: 0 */
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
