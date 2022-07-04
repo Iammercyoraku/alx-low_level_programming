8d1874a2742484786bfad11882882f9f85edadc2
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-04 16:04:43 -0700

    printing lowercase letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,9 +4,11 @@
 * followed by a new line
 * Return: Always 0 (Success)
 */
-int main(void){
+int main(void)
+{
 int ch;
-for (ch = 'a'; ch <= 'z'; ch++){
+for (ch = 'a'; ch <= 'z'; ch++)
+{
 putchar(ch);
 }
 putchar('\n');
