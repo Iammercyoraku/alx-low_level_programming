a0c2f2c8e3cfe534a2e974bc8575be77e2ca04ea
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-25 14:25:03 -0700

    reverse an array of integer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,8 @@
 /**
 * reverse_array - reverses an array of integers
 * @a: array to be reversed
-* @n: number of elements in the array */
+* @n: number of elements in the array
+*/
 void reverse_array(int *a, int n)
 {
 int i, j, tmp;
