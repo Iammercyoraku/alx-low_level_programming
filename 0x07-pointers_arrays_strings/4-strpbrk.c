0d20af187510a36d410ee5565f863d79195aed5d
Iammercyoriaku <mercyoriaku@outlook.com>
2022-07-26 22:12:42 -0700

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,14 +9,15 @@
 * or NULL if no such byte is found
 */
 char *_strpbrk(char *s, char *accept)
-{ int i, j;
+{
+int i, j;
 for (i = 0; *s != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
-{ if (*s == accept[j])
+{
+if (*s == accept[j])
 {
 return (s);
-}
 }
 s++;
 }
